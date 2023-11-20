5c193f9cf7fa3b35e072cfa44f16d3ca5c04d536
Karel R. Varona <kwaref@gmail.com>
2023-11-20 13:36:23 +0000

    feat: 3.2 done

--- index.js
+++ index.js
@@ -6,29 +6,34 @@ app.use(express.json())
 
 let persons = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
     }
 ]
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date().toString()}</p>`)
 })
 
 app.get('/api/persons', (request, response) => {
